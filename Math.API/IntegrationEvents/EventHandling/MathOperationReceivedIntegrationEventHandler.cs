715b381624162a456a202e6e462359099f25c448
Luis Molina <lmolinabe@gmail.com>
2019-09-24 21:48:12 -0600

    Add the first version of this microservices architecture workshop

---
+++ Math.API/IntegrationEvents/EventHandling/MathOperationReceivedIntegrationEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using EventBus.Abstractions;
+using Math.API.IntegrationEvents.Events;
+using Microsoft.Extensions.Logging;
+using Serilog.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Math.API.IntegrationEvents.EventHandling
+{
+    public class MathOperationReceivedIntegrationEventHandler : IIntegrationEventHandler<MathOperationReceivedIntegrationEvent>
+    {
+        private readonly ILogger<MathOperationReceivedIntegrationEventHandler> _logger;
+
+        public MathOperationReceivedIntegrationEventHandler(ILogger<MathOperationReceivedIntegrationEventHandler> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Handle(MathOperationReceivedIntegrationEvent @event)
+        {
+            using (LogContext.PushProperty("IntegrationEventContext", $"{@event.Id}-{Program.AppName}"))
+            {
+                _logger.LogInformation("----- Handling integration event: {IntegrationEventId} at {AppName} - ({@IntegrationEvent})", @event.Id, Program.AppName, @event);
+
+                //Process the math operation
+            }
+        }
+    }
+}
--- Math.API/Startup.cs
+++ Math.API/Startup.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using EventBus;
+using Math.API.IntegrationEvents.EventHandling;
+using Math.API.IntegrationEvents.Events;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Math.API
 {
@@ -23,9 +22,13 @@ namespace Math.API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var container = new ContainerBuilder();
+            container.Populate(services);
+            return new AutofacServiceProvider(container.Build());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +46,14 @@ namespace Math.API
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            ConfigureEventBus(app);
+        }
+
+        protected virtual void ConfigureEventBus(IApplicationBuilder app)
+        {
+            var eventBus = app.ApplicationServices.GetRequiredService<IEventBus>();
+            eventBus.Subscribe<MathOperationReceivedIntegrationEvent, MathOperationReceivedIntegrationEventHandler>();
         }
     }
 }
